401a1cf6717d9cbf7624f1c1d88cf5cbf833e2c8
Idilahmed <idil@mac-books-MacBook-Pro.local>
2019-07-31 13:50:18 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  
+  return (
+    <>
+    <div className="App">
+      <h1>Cycle Through Users:</h1>
+      <button>+</button>
+      <button>-</button>
+    </div>
+    </>
+  );
+}
+
+export default App;
